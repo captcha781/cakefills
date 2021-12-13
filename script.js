3a1e4aefe0da8375edc236941332caa568d791bb
captcha781 <bhuvanesh19112001@gmail.com>
2021-12-13 08:31:43 +0530

    Initial commit with section-1 completiton

---
+++ script.js
@@ -0,0 +1,46 @@
+//Animation Toggle Script
+
+var infohead = document.getElementById("infohead");
+var head = document.getElementById("header");
+var tagline = document.getElementById("tagline");
+var tags = document.getElementById("tags");
+var tags1 = document.getElementById("tags1");
+var tags2 = document.getElementById("tags2");
+
+window.onload = function () {
+  head.style.animationName = "shade";
+  infohead.style.animationName = "shade2";
+  tags.style.animation = "shade4";
+};
+var tagArray1 = [
+  "Tasty",
+  "Delightful",
+  "Yummy",
+  "Bakery",
+  "Cheerful",
+  "Pineapple"
+];
+var tagArray2 = ["Feast", "Dessert", "Tummy", "Cakery", "Cherries", "Paradise"];
+
+let j = 0;
+function taglinemaker() {
+  setInterval(() => {
+    tags1.innerHTML = tagArray1[j];
+    tags2.innerHTML = tagArray2[j];
+
+    console.log(tagArray1[j]);
+
+    tagline.style.animationName = "shade3";
+    tagline.style.animationIterationCount = "infinite";
+    // tagline.style.animationDelay = "2s";
+    tagline.style.animationDuration = "3s";
+
+    j += 1;
+    if (j > 5) {
+      j = 0;
+    }
+  }, 3000);
+  // console.log();
+}
+
+taglinemaker();
